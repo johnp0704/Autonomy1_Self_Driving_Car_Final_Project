84ae49fe1ed3b713e324a3bc53358177935fc571
Niels Keller <niels.keller1@gmail.com>
2025-11-19 14:28:14 -0500

    consts

--- interactiveSim/car.py
+++ interactiveSim/car.py
@@ -3,6 +3,20 @@ import numpy as np
 class Car:
     def __init__(self, Ts, initial_speed, mass=1300):
         # car parameters
+        m = 1300 #𝐾𝑔
+        Froll = 100 #𝑁
+        a = 0.2 #𝑁𝑠^2/𝑚2
+        b = 20 #𝑁𝑠/𝑚
+        g = 9.8 #𝑚/𝑠^2
+        fd_min = -7000 #𝑁
+        zeta = 0.95
+        eta_g = 0.8
+        eta_g = 3.8
+        rw = 0.34 #𝑚
+        F =  200 #𝑚𝑔/𝑠
+        L = 2.7 #m
+        delta_max = 0.05 #rad
+
 
         # car states 
 
