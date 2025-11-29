eb6625f48640fd50813da26c47a3d4393f3e4028
John Poirier <22johpoiri@gmail.com>
2025-11-29 18:52:06 -0500

    Added matlab code to check controller

---
+++ Task2_ControllerDesign_Check.m
@@ -0,0 +1,20 @@
+%% Define Plant
+s = tf('s');
+P = 1 / (1300*s + 31.112);
+
+%% Your Current PI Controller Gains
+Kp = 4323.888;
+Ki = 3647.3125;
+
+% PI controller
+C = Kp + Ki/s;
+
+%% Precompensator Ki / (Kp*s + Ki)
+Kpre = Ki / (Kp*s + Ki);
+
+%% Combined Controller (if needed)
+C_total = C * Kpre;
+
+%% Open PID Tuner with your PI controller structure
+% PID Tuner supports PID/PI/PIDF, so we pass 'pi'.
+pidTuner(P, C, 'pi');
